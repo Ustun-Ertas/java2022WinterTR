7de6bf1cda235605f401f56da6a89c8d262988c0
Ustun Ertas <ustunertas0@gmail.com>
2022-03-28 22:31:27 +0200

    first commit

---
+++ src/day29_staticKeyword/C01_Static.java
@@ -0,0 +1,15 @@
+package day29_staticKeyword;
+
+public class C01_Static {
+
+    String okulIsmi="Yildiz koleji";
+    static String okulTelefone="312005406";
+
+    public static void staticMethod(){
+        System.out.println("static method calisti");
+    }
+
+    public static void staticOlmayanMethod(){
+        System.out.println("static olmayan method calisti");
+    }
+}
