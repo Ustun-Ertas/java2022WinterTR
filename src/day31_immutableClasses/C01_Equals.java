b847c72aa0e98bf608e99470892086541c8354e9
Ustun Ertas <ustunertas0@gmail.com>
2022-03-30 22:50:26 +0200

    first commit

---
+++ src/day31_immutableClasses/C01_Equals.java
@@ -0,0 +1,17 @@
+package day31_immutableClasses;
+
+public class C01_Equals {
+
+    public static void main(String[] args) {
+        String str = "A";
+        str += "b";
+        str += "c";
+        System.out.println("str = " + str);
+    }
+}
+
+
+
+
+
+
