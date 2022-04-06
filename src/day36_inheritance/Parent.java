46acad7c03470c6a87b3874099aaaa50c0d88bc1
Ustun Ertas <ustunertas0@gmail.com>
2022-04-06 17:19:05 +0200

    day37

--- src/day36_inheritance/Parent.java
+++ src/day36_inheritance/Parent.java
@@ -2,9 +2,13 @@ package day36_inheritance;
 
 public class Parent {
 
-    Parent(){
+    protected Parent(){
 
-        System.out.println("Parent cons calisti");
+        System.out.println("Parent parametresiz cons calisti");
+    }
+    protected Parent(int sayi3, int sayi4){
+        System.out.println("parent 2 peremetreli cons");
+
     }
 
     protected int sayi;
