2425cebaa79c500940d4eac044f184432b3dbfbd
Ustun Ertas <ustunertas0@gmail.com>
2022-04-07 18:23:51 +0200

    day38

--- src/day38_inheritance_overriding/Isci.java
+++ src/day38_inheritance_overriding/Isci.java
@@ -1,22 +1,15 @@
 package day38_inheritance_overriding;
 
-public class Isci {
+public class Isci extends Personel{
 
-    String statu="Isci";
-    String haklar="Isciler kidem tazminati alirlar";
-    public String ikramiye="Isciler yilda bir ikramiye alir";
-    public String izin="Tum personeler yilda 4 kere izin kullanabilir";
-
-
+    public String statu="Isci";
+    public String haklar="Isciler Kidem tazminati alirlar";
+    public String ikramiye="Isciler Yilda bir ikramiye alir";
     public void mesai(){
-
-        System.out.println("Tum isciler haftalik 40 saat calisirlar");
-
+        System.out.println("Tum isciler haftalik 40 saat calisir");
     }
-    public void MaasHesapla(){
-
-        System.out.println("Tum personel 30 gun * 8 saat * 11 euro =" + (30*8*11) + "Euro maas");
+    public void maasHesapla(){
+        System.out.println("Tum isciler 30 gun * 8 saat * 11 euro = " + (30*8*11) + " Euro maas alir");
     }
-
 }
 
--- src/day38_inheritance_overriding/Ustabasi.java
+++ src/day38_inheritance_overriding/Ustabasi.java
@@ -2,52 +2,71 @@ package day38_inheritance_overriding;
 
 import day36_inheritance.UstaBasi;
 
-public class Ustabasi extends Isci {
-
-    String statu="Ustabasi";
-    String haklar="Ustabasi haftada birgun ekstra tatil hakkina sahiptir";
-
-
+public class Ustabasi extends Isci{
+    public String statu="Ustabasi";
+    public String haklar="Ustabasi haftada birgun ekstra tatil hakkina sahiptir";
     public void mesai(){
-
         System.out.println("Ariza varsa Ustabasi ekstra ucret almadan calisir");
-
     }
-    public void MaasHesapla(){
-
-        System.out.println("Tum personel 30 gun * 8 saat * 12 euro =" + (30*8*12) + "Euro maas");
+    public void maasHesapla(){
+        System.out.println("Ustabasi 30 gun * 8 saat * 12 euro = " + (30*8*12) + " Euro maas alir");
     }
-
     public static void main(String[] args) {
-       /*
-       icinde oldugumuz classin klasik halile ile bir obje olusturursak o obje ile cagirdigimiz
-       variable ve method'larda java once icinde bulundugumuz  class'a bakar
-       aradigimiz clas uyesi, icinde oldugumuz class'da varsa bize onu getirir.
-       YOKSA,
-       parent class'lara bakar, ilk buldugunu getirir.
-
-        */
-
-        Ustabasi yasin=new Ustabasi();
-        System.out.println(yasin.statu);//Ustabasi
-        System.out.println(yasin.haklar);//Ustabasi haftada birgun ekstra tatil hakkina sahiptir
-
         /*
-        Eger isci olarak ozellikleri gormek istersek
-        Class adini (Data Turu) Isci seceriz
-
+          Icinde oldugumuz Class'dan klasik haliyle bir obje olusturursak
+          o obje ile cagirdigimiz variable ve method'larda
+          Java once icinde bulundugumuz class'a bakar
+          aradigimiz class uyesi, icinde oldugumuz class'da varsa
+          bize onu getirir
+          YOKSA,
+          parent class(lar)a bakar, ilk buldugunu getirir
          */
-
-        Isci ahmet=new Ustabasi();
-        System.out.println(ahmet.statu);//Isci
-        System.out.println(ahmet.haklar);//Isciler kidem tazminati alirlar
-        System.out.println(ahmet.ikramiye);//Isciler yilda bir ikramiye alir
-        System.out.println(ahmet.izin);//Tum personeler yilda 4 kere izin kullanabilir
-
-
-
+        Ustabasi yasin=new Ustabasi();
+        System.out.println(yasin.statu); // Ustabasi
+        System.out.println(yasin.haklar); // Ustabasi haftada birgun ekstra tatil hakkina sahiptir
+        System.out.println(yasin.ikramiye); // Isciler Yilda bir ikramiye alir
+        System.out.println(yasin.izin); // Tum personel yilda 4 hafta izin kullanabilir
+        yasin.maasHesapla();
+        yasin.mesai();
+        /*
+            Eger isci olarak ozelliklerini gormek istersek
+            Class adini (Data Turu) Isci seceriz
+         */
+        Isci ahmet= new Ustabasi();
+        System.out.println(ahmet.statu); // isci
+        System.out.println(ahmet.haklar); // Isciler Kidem tazminati alirlar
+        System.out.println(ahmet.ikramiye); // Isciler Yilda bir ikramiye alir
+        System.out.println(ahmet.izin); // // Tum personel yilda 4 hafta izin kullanabilir
+        ahmet.maasHesapla(); //
+        ahmet.mesai();
+        /*
+        Siz bir objeyi hangi class'dan tanimlarsaniz o class'a ait ozelliklere sahip olur
+         */
+        Personel adem=new Ustabasi();
+        System.out.println(adem.statu); // personel
+        System.out.println(adem.haklar); // Tum personel esit haklara sahiptir
+        //System.out.println(adem.ikramiye); // CTE verir
+        System.out.println(adem.izin); // // Tum personel yilda 4 hafta izin kullanabilir
+        adem.maasHesapla();
+        adem.mesai();
+        /*
+        Personel adem=new Ustabasi(); Bu bir Ustabasi objesidir
+        Ustabasi 30 gun * 8 saat * 12 euro = 2880 Euro maas alir
+        Ariza varsa Ustabasi ekstra ucret almadan calisir
+        Personel adem=new Personel(); Bu Personel objesidir
+         */
+        /*
+        Personel adem=new Ustabasi();
+        adem'in data turu Personel'dir.
+        boylece biz adem'i ne olarak isimlendirdigimizi bilebiliriz
+        Ancak ben adem'in ustabasi oldugunu biliyorum
+        cunku constructor'i Ustabasi
+        Ama bu yazim formati ile
+        adem'in tum Personel ile birlikte sahip oldugu
+        ortak ozellikleri vurgulamak istiyorum
+        Bu kullanim seklinde olusturulan obje data turu olarak secilen class
+        ve onun parent class'larindaki variable'lari kullanabilir.
+         */
     }
-
 }
-
 
