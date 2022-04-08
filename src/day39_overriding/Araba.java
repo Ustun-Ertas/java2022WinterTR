bb997ddb8764944bcf3b344af779980d73784ff8
Ustun Ertas <ustunertas0@gmail.com>
2022-04-08 18:55:16 +0200

    day39

--- src/day39_overriding/Araba.java
+++ src/day39_overriding/Araba.java
@@ -2,17 +2,14 @@ package day39_overriding;
 
 public class Araba {
 
-    protected String hareket="Arabalar teker ile haraket eder";
-    protected String hiz="Arabalar motor gucune gore hiz yapar";
-    protected String yakit="Arabalar farkli yakitlar kullanabilir";
-    protected String marka="Arabalr uretikleri markaya sahiptir";
-
+    protected String hareket="Arabalar Teker ile hareket eder";
+    protected String hiz="Arabalar motor gucune gore hiz yaparlar";
+    protected String yakit= "Arabalar farkli yakitlar kullanabilir";
+    protected String marka="Arabalar uretildikleri markaya sahiptir";
     public void motor(){
-
-        System.out.println("Arabalar farkli markalarla motor kullanirlar");
-
+        System.out.println("Arabalar farkli markalarda motor kullanirlar");
     }
     public void yakitTuketimi(){
-        System.out.println("Arabalar motor gucu ve yakit turune gore yakit tuketirler");
+        System.out.println("arabalar motor gucu ve yakit turune gore yakit tuketirler");
     }
 }
--- src/day39_overriding/Toyota.java
+++ src/day39_overriding/Toyota.java
@@ -6,14 +6,11 @@ public class Toyota extends Araba {
     String hiz="Toyota araclar max 220 km hiz yapar";
     String marka="Toyota";
     String sirketMerkezi="Japonya";
-
     public void motor(){
-
         System.out.println("Toyota arabalar toyota marka motor kullanir");
-
     }
     public void garanti(){
-        System.out.println("Toyota araclar 10 yil garantilidir");
+        System.out.println("Toyata araclar 10 yil garantilidir");
     }
 }
 
