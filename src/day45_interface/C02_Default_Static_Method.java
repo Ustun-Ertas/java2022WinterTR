589637acb8123f97342f5ea8c25104eea3c8174c
Ustun Ertas <ustunertas0@gmail.com>
2022-04-15 18:28:11 +0200

    day45

--- src/day45_interface/C02_Default_Static_Method.java
+++ src/day45_interface/C02_Default_Static_Method.java
@@ -2,10 +2,30 @@ package day45_interface;
 
 public class C02_Default_Static_Method implements I01_Default_Static{
 
+     /*
+    Bir interface'de default veya static olarak tanimlanan ve body'si olan
+    method'larin override edilmesi mecburi DEGILDIR
+    Eger static olarak tanimlanmissa zaten override edemeyiz
+     */
+
     @Override
     public void method1() {
         System.out.println("Child clss method1");
 
+
+        /*
+    Parent Interface'deki default olarak tanimlanan method'u
+    istersek override ederiz istersek etmeyiz
+    override etmezsek, method cagrildiginda Parent Interface'deki calisir
+    override edersek, child class'daki overriding method calisir
+
+         */
+
+    }
+
+    @Override
+    public void method2() {
+        I01_Default_Static.super.method2();
     }
 
     public static void main(String[] args) {
--- src/day45_interface/I01_Default_Static.java
+++ src/day45_interface/I01_Default_Static.java
@@ -1,19 +1,17 @@
 package day45_interface;
 
 public interface I01_Default_Static {
-
-    /*
-    Interface'lerde sadece abstract method'lar bulunur body'si olan concrete bir method
-    olusturmak istedigimizde Java CTE verir
-
-    Ancak Java8 ve uzeri versiyonlarda, istisnai olarak static veta default olarak tanimlanan
-    method'larin body'si olabilir omcak burada kullanilan default keyword'u access modifier degildir
+       /*
+    Interface'lerde sadece abstract method'lar bulunur
+    body'si olan concrete bir method olusturmak istedigimizde
+    Java CTE verir
+    Ancak Java8 ve uzeri versiyonlarda, istisnai olarak
+    static veya default olarak tanimlanan method'larin body'si olabilir
+    ancak burada kullanilan default keyword'u access modifier degildir
     cunku Interface'lerde tum method'lar public ve abstract'ti
-
-
-
-
-
+    asagidaki ornekte gorulecegi gibi default olarak tanimlanmis
+    bir method'a access modifier olarak public yazabilirsiniz
+    (access modifier yazmasak da Java method'u public olarak kabul edecektir )
      */
 
     void method1();
