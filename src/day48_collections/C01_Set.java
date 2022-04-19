8f9c98878382a50e028ca444a57c045299602a68
Ustun Ertas <ustunertas0@gmail.com>
2022-04-19 22:35:33 +0200

    day48

--- src/day48_collections/C01_Set.java
+++ src/day48_collections/C01_Set.java
@@ -8,16 +8,20 @@ public class C01_Set {
     public static void main(String[] args) {
         // verilen bir array'in tekrar eden elementlerini
         // sadece bir kere yazdiran bir method olusturun
-        Integer arr[]= {3,5,4,6,3,1,2,7,8,6,1,4,2,8};
+        Integer arr[] = {3, 5, 4, 6, 3, 1, 2, 7, 8, 6, 1, 4, 2, 8};
+
         arrayiTekrarsizYazdir(arr);
     }
+
     private static void arrayiTekrarsizYazdir(Integer[] arr) {
-        Set<Integer> tekrarsizElementKumesi= new HashSet<>();
-        for (Integer each : arr
-        ) {
-            tekrarsizElementKumesi.add(each);
+        Set<Integer> tekrasizElementKumesi=new HashSet<>();
+
+        for (Integer eac:arr
+             ) {
+           tekrasizElementKumesi.add(eac);
+
         }
-        System.out.println(tekrarsizElementKumesi);
+        System.out.println(tekrasizElementKumesi);
     }
 }
 
