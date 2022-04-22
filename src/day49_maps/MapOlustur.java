1da481b4c471e605c8e3966d5d608a8ff1b403d5
Ustun Ertas <ustunertas0@gmail.com>
2022-04-22 17:29:14 +0200

    day51

--- src/day49_maps/MapOlustur.java
+++ src/day49_maps/MapOlustur.java
@@ -7,9 +7,9 @@ public class MapOlustur {
 
     public static Map<Integer,String> myMap(){
         Map<Integer,String> sinifList = new HashMap<>();
-        sinifList.put(101,"Ali, Can, Dev");
-        sinifList.put(102,"Veli, Yan, QA");
-        sinifList.put(103,"Ali, Yan, C#");
+        sinifList.put(101,"Ali, Can, Dev,1453");
+        sinifList.put(102,"Veli, Yan, QA,2002");
+        sinifList.put(103,"Ali, Yan, C#,2021");
         return sinifList;
     }
 }
