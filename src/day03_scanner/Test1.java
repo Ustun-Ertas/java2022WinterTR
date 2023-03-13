674369b4e60a9de83957d0e6fbcd63bb2b79303e
Ustun Ertas <ustunertas0@gmail.com>
2023-03-13 19:01:44 +0300

    project

---
+++ src/day03_scanner/Test1.java
@@ -0,0 +1,12 @@
+package day03_scanner;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int x = 12;
+        int y = x++ *2 + ++x;
+        System.out.println(y);
+    }
+}
+
+
